13090ffdccfeb4477e1aed072e11242c6faf62e9
itobelisk <shantkh@mail.com>
2021-05-08 10:33:49 +0400

    08-05-2021

--- src/main/java/com/auth/server/DreamShopAuthServerApplication.java
+++ src/main/java/com/auth/server/DreamShopAuthServerApplication.java
@@ -3,16 +3,21 @@ package com.auth.server;
 import com.auth.server.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.validation.constraints.NotNull;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
-public class DreamShopAuthServerApplication {
+@EnableEurekaClient
+@EnableJpaRepositories
+public class DreamShopAuthServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DreamShopAuthServerApplication.class, args);
@@ -27,6 +32,11 @@ public class DreamShopAuthServerApplication {
             }
         };
 
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DreamShopAuthServerApplication.class);
     }
 
 }
